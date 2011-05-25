e2a39c20c48130795cad0e61c12d6e8dcd04dc49
David Zhang <dmonopoly10@gmail.com>
2011-05-24 20:18:41 -0400

    first commit - created the app with proper Gemfile

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'rake', '~> 0.8.7'
+
+group :development, :test do
+    gem "rspec-rails", ">= 2.0.0"
+    gem "cucumber-rails", ">= 0.3.2"
+    gem "webrat", ">= 0.7.2"
+    gem "database_cleaner", ">= 0.5.0"
+    gem "selenium-client", ">= 1.2.18"
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showtime::Application.config.secret_token = '5706436b52c7a3d517fb859e3482def9ea5065f7a638e23c72103befea4c3a0d6e7009c6b23d66a6b345da9a2a7f7977e1e40c75429bce9e579b7888efeb4d9b'
