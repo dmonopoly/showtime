6176f1e68d2d13c40e5724a19b2b3e72952cc1d4
David Zhang <dmonopoly10@gmail.com>
2011-05-26 20:26:15 -0400

    bottom p298

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110526031134) do
+ActiveRecord::Schema.define(:version => 20110527000612) do
+
+  create_table "genres", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "movies", :force => true do |t|
     t.date     "showtime_date"
